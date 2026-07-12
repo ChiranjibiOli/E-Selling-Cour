3ae84da2f6cd1cf7b9cbe0edf503042bb8ded1b0
Chiranjibi Oli <162393753+ChiranjibiOli@users.noreply.github.com>
2026-07-12 21:58:30 +0545

    Bootstrap explicit database connection for landing page

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+
+require_once '../app/config/database.php';
+$conn = database_connection();
+
 require_once '../app/views/layouts/header.php';
 require_once '../app/views/layouts/navbar.php';
 require_once '../app/views/home/landing.php';
