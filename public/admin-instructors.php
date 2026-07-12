ed2d51d14fdd6cb880331f2dd6189d1c0241ac3f
Chiranjibi Oli <162393753+ChiranjibiOli@users.noreply.github.com>
2026-07-12 19:57:08 +0545

    Route instructor state changes through hardened controller

--- public/admin-instructors.php
+++ public/admin-instructors.php
@@ -1,5 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
+if (($_SERVER['REQUEST_METHOD'] ?? 'GET') === 'POST') {
+    require_once '../app/actions/admin_instructor_action.php';
+    exit;
+}
 
 require_once '../app/views/admin/instructors.php';
-
