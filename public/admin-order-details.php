cf69f4e7126451e9ed89f704460e017b8c37f641
Chiranjibi Oli <162393753+ChiranjibiOli@users.noreply.github.com>
2026-07-12 19:56:07 +0545

    Route admin order posts through hardened controller

--- public/admin-order-details.php
+++ public/admin-order-details.php
@@ -1,3 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
+if (($_SERVER['REQUEST_METHOD'] ?? 'GET') === 'POST') {
+    require_once '../app/actions/admin_order_action.php';
+    exit;
+}
+
 require_once '../app/views/admin/order_details.php';
