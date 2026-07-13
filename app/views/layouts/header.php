83049d2e0620a1fb64451ceba8401dbae2c06df3
Chiranjibi Oli <162393753+ChiranjibiOli@users.noreply.github.com>
2026-07-13 11:35:42 +0545

    Load calmer course card theme

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -51,6 +51,8 @@ $documentTitle = isset($pageTitle) && trim((string) $pageTitle) !== ''
         <link rel="stylesheet" href="assets/css/panel-final.css?v=2" data-panel-style="panel-final">
     <?php endif; ?>
 
+    <link rel="stylesheet" href="assets/css/components/course-card-theme.css?v=1">
+
     <style>
         .sr-only {
             position: absolute !important;
