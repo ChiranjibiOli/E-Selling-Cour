968a3dfda228b4196d69ca42b356f48ac0fbe22b
Chiranjibi Oli <162393753+ChiranjibiOli@users.noreply.github.com>
2026-07-13 17:19:38 +0545

    Remove footer from Contact page

--- public/contact.php
+++ public/contact.php
@@ -2,5 +2,6 @@
 require_once '../app/views/layouts/header.php';
 require_once '../app/views/layouts/navbar.php';
 require_once '../app/views/home/contact.php';
-require_once '../app/views/layouts/footer.php';
 ?>
+</body>
+</html>
