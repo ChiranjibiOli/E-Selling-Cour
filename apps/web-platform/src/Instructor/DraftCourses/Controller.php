e6c903523ac3b2ae5d9b7ffee7f921f6c534bdd7
Chiranjibi Oli <162393753+ChiranjibiOli@users.noreply.github.com>
2026-07-27 20:36:59 +0545

    Open real draft course queue

--- apps/web-platform/src/Instructor/DraftCourses/Controller.php
+++ apps/web-platform/src/Instructor/DraftCourses/Controller.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 use CourseHub\WebPlatform\Shared\Http\Request;
+use CourseHub\WebPlatform\Shared\Http\Response;
 use CourseHub\WebPlatform\Shared\Room\RoomRuntime;
 
-return static function (Request $request) {
+return static function (Request $request): Response {
     RoomRuntime::authorize(__DIR__, $request);
-    return RoomRuntime::render(__DIR__, RoomRuntime::load(__DIR__, $request));
+    return Response::redirect('/instructor/courses?status=draft');
 };
