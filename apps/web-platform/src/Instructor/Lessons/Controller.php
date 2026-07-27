45268bbe851f232fb2e835b2e7f4596751f74dc2
Chiranjibi Oli <162393753+ChiranjibiOli@users.noreply.github.com>
2026-07-27 20:00:13 +0545

    Move lesson editing into complete authoring

--- apps/web-platform/src/Instructor/Lessons/Controller.php
+++ apps/web-platform/src/Instructor/Lessons/Controller.php
@@ -3,9 +3,17 @@
 declare(strict_types=1);
 
 use CourseHub\WebPlatform\Shared\Http\Request;
+use CourseHub\WebPlatform\Shared\Http\Response;
 use CourseHub\WebPlatform\Shared\Room\RoomRuntime;
 
-return static function (Request $request) {
+return static function (Request $request): Response {
     RoomRuntime::authorize(__DIR__, $request);
-    return RoomRuntime::render(__DIR__, RoomRuntime::load(__DIR__, $request));
+    $courseId = filter_var($request->query['course'] ?? 0, FILTER_VALIDATE_INT);
+    $destination = '/instructor/courses/create';
+    if ($courseId !== false && $courseId > 0) {
+        $destination .= '?course=' . (int) $courseId . '#curriculum';
+    } else {
+        $destination .= '#curriculum';
+    }
+    return Response::redirect($destination);
 };
